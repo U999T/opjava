8ca45d1c4b316d4a3a1f2f277027228c5185bd0b
T <tdottavio@itsmeccanicabruzzo.eu>
2023-06-19 12:01:13 +0200

    Esercizio product invectory

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,85 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-class Student {
+class Product {
+    private String id;
     private String name;
-    private List<Integer> grades;
+    private double price;
 
-    public Student(String name) {
+    public Product(String id, String name, double price) {
+        this.id = id;
         this.name = name;
-        this.grades = new ArrayList<>();
+        this.price = price;
     }
 
-    public void addGrade(int grade) {
-        grades.add(grade);
-    }
-
-    public double getAverageGrade() {
-        if (grades.isEmpty()) {
-            return 0.0;
-        }
-
-        int sum = 0;
-        for (int grade : grades) {
-            sum += grade;
-        }
-
-        return (double) sum / grades.size();
+    public String getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        // Create two Student objects
-        Student student1 = new Student("Luca");
-        Student student2 = new Student("Rosa");
+        Map<String, Product> inventory = new HashMap<>();
 
-        // Add grades for each student
-        student1.addGrade(29);
-        student1.addGrade(28);
-        student1.addGrade(30);
+        // Adding products to the inventory
+        Product product1 = new Product("01", "Washing Machine", 445.00);
+        Product product2 = new Product("02", "Television", 910.00);
+        Product product3 = new Product("03", "Electric Shaver", 329.00);
 
-        student2.addGrade(27);
-        student2.addGrade(30);
-        student2.addGrade(28);
+        inventory.put(product1.getId(), product1);
+        inventory.put(product2.getId(), product2);
+        inventory.put(product3.getId(), product3);
 
-        // Calculate and display the average grade for each student
-        double averageGrade1 = student1.getAverageGrade();
-        double averageGrade2 = student2.getAverageGrade();
+        // Display the total number of products in the inventory
+        int totalProducts = inventory.size();
+        System.out.println("Total number of products: " + totalProducts);
 
-        System.out.println("Average grade for " + student1.getName() + ": " + averageGrade1);
-        System.out.println("Average grade for " + student2.getName() + ": " + averageGrade2);
-
-        // Compare the average grades and display the student with the higher average grade
-        if (averageGrade1 > averageGrade2) {
-            System.out.println(student1.getName() + " has a higher average grade.");
-        } else if (averageGrade2 > averageGrade1) {
-            System.out.println(student2.getName() + " has a higher average grade.");
+        // Display details of a specific product by providing its ID
+        String productId = "02";
+        Product product = inventory.get(productId);
+        if (product != null) {
+            System.out.println("Product details: " + product.getName() + " - Price: €" + product.getPrice());
         } else {
-            System.out.println("Both students have the same average grade.");
+            System.out.println("Product not found with ID: " + productId);
         }
+
+        // Remove a product from the inventory by providing its ID
+        String productIdToRemove = "03";
+        Product removedProduct = inventory.remove(productIdToRemove);
+        if (removedProduct != null) {
+            System.out.println("Product removed: " + removedProduct.getName());
+        } else {
+            System.out.println("Product not found with ID: " + productIdToRemove);
+        }
+
+        // Update the price of a product by providing its ID
+        String productIdToUpdate = "01";
+        double newPrice = 515.00;
+        Product productToUpdate = inventory.get(productIdToUpdate);
+        if (productToUpdate != null) {
+            productToUpdate.setPrice(newPrice);
+            System.out.println("Product price updated. New price: €" + productToUpdate.getPrice());
+        } else {
+            System.out.println("Product not found with ID: " + productIdToUpdate);
+        }
+
+        // Calculate and display the total value of the inventory (the sum of all product prices)
+        double totalValue = 0.0;
+        for (Product p : inventory.values()) {
+            totalValue += p.getPrice();
+        }
+        System.out.println("Total value of the inventory: €" + totalValue);
     }
 }
